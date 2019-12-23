835282ff20abf7f398ebe3ee7bb5e6ceedf458d1
zzyyxxww <guidotarsia@gmail.com>
2019-12-23 16:14:38 -0300

    Add config and test docs

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -1,3 +1,24 @@
 
 module.exports = {
+  title: 'Learn development',
+  description: 'A website to learn development',
+  themeConfig: {
+    sidebar: [{
+      path: '/learn/web/',
+      children: [
+        'http',
+        'client-vs-server-frameworks'
+      ]
+    }]
+    // sidebar: {
+    //   '/learn/web': [
+    //     '/learn/web/client-vs-server-frameworks'
+    //   ],
+    //   '/learn/': [
+    //     'web',
+    //   ],
+    //   '/': [
+    //   ],
+    // }
+  }
 }
